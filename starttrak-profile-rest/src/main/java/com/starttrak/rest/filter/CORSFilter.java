c1c89ecaeb0df6af8c3fa58e28cc2f6036f52540
sergii.mavrov <serg.mavrov@gmail.com>
2016-02-11 21:18:45 +0100

    fast development

---
+++ starttrak-profile-rest/src/main/java/com/starttrak/rest/filter/CORSFilter.java
@@ -0,0 +1,30 @@
+package com.starttrak.rest.filter;
+
+import org.jboss.logging.Logger;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+
+    private final static Logger logger = Logger.getLogger(CORSFilter.class);
+
+    public CORSFilter() {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        logger.info("CORS filter initiated");
+    }
+}
