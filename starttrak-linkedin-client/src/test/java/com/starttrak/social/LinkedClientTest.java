bedc79ab5dca3c83b4b1c8169822aefab1ede3ae
Sergii Mavrov <serg.mavrov@gmail.com>
2016-02-14 16:06:13 +0100

    fast development

--- starttrak-linkedin-client/src/test/java/com/starttrak/social/LinkedClientTest.java
+++ starttrak-linkedin-client/src/test/java/com/starttrak/social/LinkedClientTest.java
@@ -21,10 +21,7 @@ public class LinkedClientTest {
     private final static Logger logger = Logger.getLogger(LinkedClientTest.class);
 
     private final static String SERGII_ACCESS_TOKEN =
-            "AQVz3r_gUlFS_vGFXDXbUkYbCwhuDxPwPT2e9S3dYTtde8mDqQAaP1a8IWQ" +
-                    "ekdWfRYGHll7__yklyo033NEweAQd6hZGIfHxICC2bB-bgmAC7k" +
-                    "tmzYTbd_qo9adhfg6Ug3W59pX3GD2fhjrQcJPYdev8qYlYIjluD" +
-                    "Qaa1c9ugGm-hA4Al6A";
+            "AQXEEdn5zXA1EL4LKRli5B2Hno5HYnWGjwn6tc1MNqn_rmQWqciHxYt3ZNk94kOUCcxUdiU_90E364oNXVVohNP-_NIqkOyTkUwuZ8BH51WVE_jPo2ho93OYDQVl822j1IrW7d3tr4QNzVD29m0hPsQBbtxGcRhxW3WF15CHpYuLGlTYbs8";
 
     private final static String DENIS_ACCESS_TOKEN =
             "AQUMXQTBS6mxAH9I4-_HUArMQ_bsAKDRRYJ_R2criIDWgV_VfJhaHxQebZEO" +
@@ -54,9 +51,10 @@ public class LinkedClientTest {
                 "AFRMkAlGLRyqmT3BikRZGcDkQUAN0SuqNPRpFArF9L8cmSTliPRR1alFL" +
                 "uKAuKpc3gBAktAktuKluKgBtgByAWDXFn3ZuF2OtJbgR0mI1W", profile.getPictureUrl().get());
         assertTrue(profile.getCityName().isPresent());
+        assertEquals("Düsseldorf Area", profile.getCityName().get());
         assertFalse(profile.getRegion().isPresent());
         assertTrue(profile.getCountry().isPresent());
-
+        assertEquals("Germany", profile.getCountry().get());
     }
 
     @Test
